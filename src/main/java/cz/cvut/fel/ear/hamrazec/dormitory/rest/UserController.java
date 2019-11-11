820d85103d39301d7b2aa30297423b0d7f9a993b
Sandra Hamráková <hamrasan@fel.cvut.cz>
2019-11-11 23:57:57 +0100

    controller

---
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/rest/UserController.java
@@ -0,0 +1,11 @@
+package cz.cvut.fel.ear.hamrazec.dormitory.rest;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class UserController {
+
+
+}
