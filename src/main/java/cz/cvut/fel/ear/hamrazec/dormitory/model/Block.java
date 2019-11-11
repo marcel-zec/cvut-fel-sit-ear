4d81106159abcb08ce0b82b6357a0f45e685f157
Sandra Hamráková <hamrasan@fel.cvut.cz>
2019-11-11 22:35:10 +0100

    Entity getters, setters, constructors, attributes.

--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Block.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Block.java
@@ -1,7 +1,59 @@
 package cz.cvut.fel.ear.hamrazec.dormitory.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
-public class Block {
+public class Block  extends AbstractEntity{
+
+    @Basic(optional = false)
+    @Column(nullable = false, length = 3)
+    @Size(max = 3, min = 1, message = "Name of block is in incorrect format.")
+    private String name;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    @Size(max = 255, min = 5)
+    private String adress;
+
+    @ManyToMany
+    @OrderBy("username")
+    private List<Manager> managers;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "room_id")
+    private List<Room> rooms;
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public List<Manager> getManagers() {
+        return managers;
+    }
+
+    public void setManagers(List<Manager> managers) {
+        this.managers = managers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
 }
--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Student.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Student.java
@@ -1,7 +1,76 @@
 package cz.cvut.fel.ear.hamrazec.dormitory.model;
 
-import javax.persistence.Entity;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 @Entity
-public class Student {
+public class Student extends User {
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private String university;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    @Past
+    private LocalDate birth;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    @Size(max = 26, min = 16, message = "Bank account is in incorrect format. Please use IBAN format.")
+    private String bankAccountNumber;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    @FutureOrPresent
+    private LocalDate endOfStudy;
+
+    @Enumerated(EnumType.STRING)
+    @Type(type = "org.thoughts.on.java.model.EnumTypePostgreSql")
+    private Gender gender;
+
+    public Student(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public LocalDate getEndOfStudy() {
+        return endOfStudy;
+    }
+
+    public void setEndOfStudy(LocalDate endOfStudy) {
+        this.endOfStudy = endOfStudy;
+    }
 }
