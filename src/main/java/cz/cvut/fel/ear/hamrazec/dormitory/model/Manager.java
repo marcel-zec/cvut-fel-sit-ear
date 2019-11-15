5517516a5fd04764c2961120d7132de05dd4d88c
Sandra Hamráková <hamrasan@fel.cvut.cz>
2019-11-15 17:47:51 +0100

    manager update

--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Manager.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Manager.java
@@ -1,9 +1,13 @@
 package cz.cvut.fel.ear.hamrazec.dormitory.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Manager.findByWorkerNumber", query = "SELECT m FROM Manager m WHERE m.workerNumber = :workerNumber")
+})
 public class Manager extends User {
 
     @Basic(optional = false)
@@ -18,26 +22,16 @@ public class Manager extends User {
     private List<Block> blocks;
 
 
-    public Integer getWorkerNumber() {
+    public Integer getWorkerNumber() { return workerNumber; }
 
-        return workerNumber;
-    }
+    public void setWorkerNumber(Integer workerNumber) { this.workerNumber = workerNumber; }
 
+    public List<Block> getBlocks() { return blocks; }
 
-    public void setWorkerNumber(Integer workerNumber) {
+    public void setBlocks(List<Block> blocks) { this.blocks = blocks; }
 
-        this.workerNumber = workerNumber;
-    }
-
-
-    public List<Block> getBlocks() {
-
-        return blocks;
-    }
-
-
-    public void setBlocks(List<Block> blocks) {
-
-        this.blocks = blocks;
+    public void addBlock(Block block){
+        if (this.blocks == null) this.blocks = new ArrayList<Block>();
+        this.blocks.add(block);
     }
 }
