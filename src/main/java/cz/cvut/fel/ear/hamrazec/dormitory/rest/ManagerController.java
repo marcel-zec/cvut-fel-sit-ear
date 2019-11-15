d9d1ac9f6619a67c5f6fb859a11ca4c57365ceb5
Sandra Hamráková <hamrasan@fel.cvut.cz>
2019-11-15 16:20:09 +0100

    manager service, controller

---
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/rest/ManagerController.java
@@ -0,0 +1,60 @@
+package cz.cvut.fel.ear.hamrazec.dormitory.rest;
+
+import cz.cvut.fel.ear.hamrazec.dormitory.model.Manager;
+import cz.cvut.fel.ear.hamrazec.dormitory.service.ManagerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/managers")
+@Validated
+public class ManagerController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ManagerController.class);
+    private ManagerService managerService;
+
+
+    @Autowired
+    public ManagerController(ManagerService managerService) {
+
+        this.managerService = managerService;
+    }
+
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Manager> getManagers() {
+
+        return managerService.findAll();
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Manager getManager(@PathVariable Long id) {
+
+        return managerService.find(id);
+    }
+
+
+    @DeleteMapping(value = "/{managerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteManager(@PathVariable Long managerId) {
+
+        managerService.delete(managerId);
+    }
+
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void createManager(@RequestBody Manager manager) {
+
+        managerService.create(manager);
+        LOG.info("Category with id {} created", manager.getId());
+    }
+}
