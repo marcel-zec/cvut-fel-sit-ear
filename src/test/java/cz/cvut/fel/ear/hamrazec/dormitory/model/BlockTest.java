7251625fa1721b0bf317866ba8b864812fc2aa37
Marcel Žec <zecmarce@fel.cvut.cz>
2019-11-16 21:26:32 +0100

    blocktest

--- src/test/java/cz/cvut/fel/ear/hamrazec/dormitory/model/BlockTest.java
+++ src/test/java/cz/cvut/fel/ear/hamrazec/dormitory/model/BlockTest.java
@@ -3,6 +3,8 @@ package cz.cvut.fel.ear.hamrazec.dormitory.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BlockTest {
     Block block;
 
@@ -12,7 +14,30 @@ public class BlockTest {
     }
 
     @Test
-    public void addManagerToBlockWorks(){
+    public void addRoomToBlock_WorksCorrect(){
+        block.addRoom(new Room());
+        assertEquals("Add room not working",1,block.getRooms().size());
+    }
 
+    @Test
+    public void addManagerToBlock_WorksCorrect(){
+        block.addManager(new Manager());
+        assertEquals("Add manager not working",1,block.getManagers().size());
+    }
+
+    @Test
+    public void addManagerAlreadyAdded_NothingHappen(){
+        Manager manager = new Manager();
+        block.addManager(manager);
+        block.addManager(manager);
+        assertEquals("Added manager two times",1,block.getManagers().size());
+    }
+
+    @Test
+    public void addRoomAlreadyAdded_NothingHappen(){
+        Room room = new Room();
+        block.addRoom(room);
+        block.addRoom(room);
+        assertEquals("Added room two times",1,block.getRooms().size());
     }
 }
