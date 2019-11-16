18fa660da47c0d6cf49e065750e8bb09ba751a54
Marcel Žec <zecmarce@fel.cvut.cz>
2019-11-16 21:00:51 +0100

    block test

---
+++ src/test/java/cz/cvut/fel/ear/hamrazec/dormitory/model/BlockTest.java
@@ -0,0 +1,18 @@
+package cz.cvut.fel.ear.hamrazec.dormitory.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlockTest {
+    Block block;
+
+    @Before
+    public void init(){
+        this.block = new Block();
+    }
+
+    @Test
+    public void addManagerToBlockWorks(){
+
+    }
+}
