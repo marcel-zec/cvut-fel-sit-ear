c3a44e3baa0c5b77c2faf58d62508c4c3297771a
Marcel Žec <zecmarce@fel.cvut.cz>
2019-11-17 14:16:04 +0100

    statusy ku ubytovaniu a rezervaciam

--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Status.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/model/Status.java
@@ -1,16 +1,27 @@
 package cz.cvut.fel.ear.hamrazec.dormitory.model;
 
 public enum Status {
-    PENDING("STATUS_PENDING"), AGREED("STATUS_AGREED");
+    //accommodation
+    ACTIVE("ACTIVE"),
+    CANCELED("CANCELED"),
+    ENDED("ENDED"),
+    //reservation
+    PENDING("PENDING_APPROVAL"),
+    APPROVED("RESERVATION_APPROVED"),
+    RESERVATION_CANCELED("RESERVATION_CANCELED");
 
     private final String status;
 
+
     Status(String status) {
+
         this.status = status;
     }
 
+
     @Override
     public String toString() {
+
         return status;
     }
 }
