6fbeccf1118fa81055f78337971f8a35eb006958
Sandra Hamráková <hamrasan@fel.cvut.cz>
2020-01-01 21:25:14 +0100

    create Reservation

---
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/rest/ReservationController.java
@@ -0,0 +1,59 @@
+package cz.cvut.fel.ear.hamrazec.dormitory.rest;
+
+import cz.cvut.fel.ear.hamrazec.dormitory.exception.NotFoundException;
+import cz.cvut.fel.ear.hamrazec.dormitory.model.Reservation;
+import cz.cvut.fel.ear.hamrazec.dormitory.service.ReservationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
+@Validated
+public class ReservationController {
+
+    private ReservationService reservationService;
+    private static final Logger LOG = LoggerFactory.getLogger(ReservationController.class);
+
+
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping(value = "block/{blockName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Reservation> getReservations(@PathVariable String blockName) {
+
+        return reservationService.findAll(blockName);
+    }
+
+    @PostMapping(value = "student/{student_id}/room/{room_id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void createAccommodation(@RequestBody Reservation reservation, @PathVariable Long student_id, @PathVariable Long room_id) {
+
+        try {
+            reservationService.createNewReservation(reservation, student_id, room_id);
+            LOG.info("Reservation with id {} created", reservation.getId());
+        }catch (NotFoundException e) {
+            //TODO - exceptions
+        }
+    }
+
+    @PostMapping(value = "student/{student_id}/block/{blockName}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void createAccommodationRandom(@RequestBody Reservation reservation, @PathVariable Long student_id, @PathVariable String blockName) {
+
+        try {
+            reservationService.createNewReservationRandom(reservation, student_id, blockName);
+            LOG.info("Reservation with on room {} created", reservation.getRoom());
+        }catch (NotFoundException e) {
+            //TODO - exceptions
+        }
+    }
+
+}
--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/service/ReservationService.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/service/ReservationService.java
@@ -1,15 +1,17 @@
 package cz.cvut.fel.ear.hamrazec.dormitory.service;
 
 import cz.cvut.fel.ear.hamrazec.dormitory.dao.ReservationDao;
-import cz.cvut.fel.ear.hamrazec.dormitory.model.Accommodation;
-import cz.cvut.fel.ear.hamrazec.dormitory.model.Reservation;
-import cz.cvut.fel.ear.hamrazec.dormitory.model.Status;
+import cz.cvut.fel.ear.hamrazec.dormitory.dao.RoomDao;
+import cz.cvut.fel.ear.hamrazec.dormitory.dao.StudentDao;
+import cz.cvut.fel.ear.hamrazec.dormitory.exception.NotFoundException;
+import cz.cvut.fel.ear.hamrazec.dormitory.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,15 +19,30 @@ public class ReservationService {
 
     private ReservationDao reservationDao;
     private AccommodationService accommodationService;
+    private StudentDao studentDao;
+    private RoomDao roomDao;
+    private RoomService roomService;
 
     @Autowired
-    public ReservationService(ReservationDao reservationDao, AccommodationService accommodationService) {
+    public ReservationService(ReservationDao reservationDao, AccommodationService accommodationService, StudentDao studentDao, RoomDao roomDao, RoomService roomService) {
 
         this.reservationDao = reservationDao;
         this.accommodationService = accommodationService;
+        this.studentDao = studentDao;
+        this.roomDao = roomDao;
+        this.roomService = roomService;
     }
 
     public List<Reservation> findAll() { return reservationDao.findAll();  }
+
+    public List<Reservation> findAll(String blockName) {
+
+        List<Reservation> reservationsInBlock = new ArrayList<>();
+        for (Reservation reservation: findAll()) {
+            if (reservation.getRoom().getBlock().getName().equals(blockName)) reservationsInBlock.add(reservation);
+        }
+        return reservationsInBlock;
+    }
 
     public Reservation find(Long id) {
         return reservationDao.find(id);
@@ -36,13 +53,58 @@ public class ReservationService {
         setStatusAndUnusualEnd(reservation,Status.RES_CANCELED);
     }
 
-    //TODO - reserve na konkretnu izbu a nahodnu izbu
+    @Transactional
+    public void createNewReservation(Reservation reservation, Long student_id, Long room_id) throws NotFoundException {
+
+        Student student = studentDao.find(student_id);
+        Room room = roomDao.find(room_id);
+        reservation.setStudent(student);
+        reservation.setRoom(room);
+        if (student == null || room == null) throw new NotFoundException();
+
+        if (roomService.findFreeConcreteRoom(room.getBlock().getName(),reservation.getDateStart(),
+                reservation.getDateEnd(),reservation.getRoom().getRoomNumber()))
+        {
+            reservation.setStatus(Status.RES_APPROVED);
+            room.addReservation(reservation);
+            student.addReservation(reservation);
+            reservationDao.persist(reservation);
+            studentDao.update(student);
+            roomDao.update(room);
+        }
+
+    }
+
+    @Transactional
+    public void createNewReservationRandom(Reservation reservation, Long student_id, String blockName) throws NotFoundException {
+
+        Student student = studentDao.find(student_id);
+        reservation.setStudent(student);
+        if (student == null) throw new NotFoundException();
+
+        List<Room> freeRooms = roomService.findFreeRooms(blockName, reservation.getDateStart(), reservation.getDateEnd());
+        if (freeRooms !=null){
+            reservation.setStatus(Status.RES_APPROVED);
+            Room room = freeRooms.get(0);
+            reservation.setRoom(room);
+            room.addReservation(reservation);
+            student.addReservation(reservation);
+            reservationDao.persist(reservation);
+            studentDao.update(student);
+            roomDao.update(room);
+        }
+    }
 
     @Scheduled(cron = "0 0 3 * * *", zone = "CET")
     @Transactional
     public void updateExpired() {
 
-        //TODO - reservation
+        for (Reservation reservation: findAll()) {
+            if (reservation.getDateEnd().isBefore(LocalDate.now())) {
+                reservation.setStatus(Status.RES_CANCELED);
+                reservationDao.update(reservation);
+            }
+        }
     }
 
     private void setStatusAndUnusualEnd(Reservation reservation, Status status){
