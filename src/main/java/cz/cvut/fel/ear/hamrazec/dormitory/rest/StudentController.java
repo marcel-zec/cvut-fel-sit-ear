747bf9c57a8d53ab57a13032f373dcfd09d77464
Marcel Žec <zecmarce@fel.cvut.cz>
2020-01-05 17:34:13 +0100

    Merge branch 'master' of D:\CODES\JAVA\EAR\EAR_semestralka with conflicts.

--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/rest/StudentController.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/rest/StudentController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/students")
 @Validated
+@PreAuthorize("hasRole('ROLE_STUDENT')")
 public class StudentController {
 
     //TODO - pristup len pre managera a superuser
