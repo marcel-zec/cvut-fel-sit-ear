4f6476021cc4005f62a87b0d956a102496b22e11
Sandra Hamráková <hamrasan@fel.cvut.cz>
2020-01-05 11:53:27 +0100

    ..

--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/rest/LoginController.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/rest/LoginController.java
@@ -1,14 +1,8 @@
 package cz.cvut.fel.ear.hamrazec.dormitory.rest;
 
-import cz.cvut.fel.ear.hamrazec.dormitory.model.User;
-import cz.cvut.fel.ear.hamrazec.dormitory.security.DefaultAuthenticationProvider;
-import cz.cvut.fel.ear.hamrazec.dormitory.service.LoginService;
+import cz.cvut.fel.ear.hamrazec.dormitory.service.security.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/service/security/LoginService.java
+++ src/main/java/cz/cvut/fel/ear/hamrazec/dormitory/service/security/LoginService.java
@@ -1,10 +1,9 @@
-package cz.cvut.fel.ear.hamrazec.dormitory.service;
+package cz.cvut.fel.ear.hamrazec.dormitory.service.security;
 
 import cz.cvut.fel.ear.hamrazec.dormitory.security.DefaultAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
